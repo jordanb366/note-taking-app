1a4e63125189780aa81afe028e7ace978226da87
Jordan <jordan.b3360@gmail.com>
2022-08-23 22:08:37 -0500

    added notes post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const notesDB = require("./db/db.json");
 
 const PORT = process.env.port || 3001;
 
@@ -22,6 +23,15 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => res.json(notesDB));
+
+app.post("/api/notes", (req, res) => {
+  const { body } = req;
+  notesDB.push(body);
+  res.json(notesDB);
+  console.log(typeof body);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)

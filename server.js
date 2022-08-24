74999b95694f4f5d81ef57c7ad44c2e82cd4a780
Jordan <jordan.b3360@gmail.com>
2022-08-23 21:54:35 -0500

    added notes route

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// GET Route for homepage
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
